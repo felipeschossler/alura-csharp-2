6b9d373f5fff03da479e322157991f89ed232b8e
Felipe Schossler <felipealvesschossler@gmail.com>
2018-11-26 23:33:01 -0200

    Finished chapter 4 not understanding well

---
+++ ByteBank/05-ByteBank/ContaCorrente.cs
@@ -0,0 +1,45 @@
+﻿// using _05_ByteBank;
+
+namespace _05_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        public double saldo = 100;
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo <= valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo <= valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+
+}
---
+++ ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cliente gabriela = new Cliente(); // cria o objeto gabriela baseado na classe Cliente
+
+            gabriela.nome = "Gabriela";
+            gabriela.profissao = "Desenvolvedora C#";
+            gabriela.cpf = "434.562.878-20";
+
+            ContaCorrente conta = new ContaCorrente(); // cria o objeto conta baseado na classe ContaCorrente
+
+            conta.titular = gabriela; // seta a conta.titular(que é uma referência ao objeto cliente) como o objeto gabriela. Isso é feito para vincular os dados entre as classes. 
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+
+            Console.WriteLine(conta.titular.nome); // acessando o campo nome pelo objeto conta(classe ContaCorrente) que lá tem uma referência ao objeto Cliente
+            Console.WriteLine(gabriela.nome); // acessando o campo nome pelo objeto gabriela(classe Cliente) diretamente.
+                
+        }
+    }
+}
