83f3aaf5b4324f6020a584253ad442e94b333c9f
Felipe Schossler <felipealvesschossler@gmail.com>
2018-11-28 00:13:22 -0200

    finished chapter 5

---
+++ ByteBank/06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,77 @@
+﻿// using _05_ByteBank;
+
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        //declaração dos campos
+        private Cliente _titular;
+        private int _agencia;
+        private int _numero;
+        private double _saldo = 100;
+
+        //método Titular que referencia a classe(objeto) Cliente
+        public Cliente Titular{ get; set;}
+           
+        //método Agencia
+        public int Agencia { get; set; }
+        //método Número
+        public int Numero { get; set; }
+
+        //método Saldo
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        //método Sacar
+        public bool Sacar(double valor)
+        {
+            if (this._saldo <= valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+                return true;
+            }
+        }
+
+        //método Depositar
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        //método Transferir
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo <= valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+
+
+}
---
+++ ByteBank/06-ByteBank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //nesta aula foi evidenciado o conceito de encapsulamento e o benefício do uso de getters e setters ao invés de alterar os campos diretamente, pois temos mais poder para 
+            //validá-los e manipulá-los.
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            //iniciando variáveis para serem usadas no WriteLine conta.titular.Nome
+            cliente.Nome = "Guilherme";
+            cliente.Cpf = "434.564.879-20";
+            cliente.Profissao = "Desenvolvedor";
+
+            conta.Titular = cliente;
+            conta.Saldo = -10;//não funciona, pois a lógica do setter impede isso.
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+
+        }
+    }
+}
